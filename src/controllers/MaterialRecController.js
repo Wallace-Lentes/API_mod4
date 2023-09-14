e29310d8950e8c215a3b9eba5da266210ce357e6
wallace lentes <wallace.lentes@gmail.com>
2023-09-13 23:41:03 -0300

    feat: criação do arquivo para as rotas dos materiais.

--- src/controllers/MaterialRecController.js
+++ src/controllers/MaterialRecController.js
@@ -1,4 +1,18 @@
-class MaterialRecController {
+import MateriaisDAO from "../DAO/MateriaisDAO.js"
 
+class MaterialRecController {
+    /**
+   * Método para contralização de rotas no controller
+   * @param {express} app 
+   */
+    static rotas(app){
+
+        app.get("/material", async (req, res) => {
+            const material = await MateriaisDAO.buscarTododsOsMateriais()
+            console.log(material)
+            res.status(200).json(material)
+          })
+
+    }
 }
 export default MaterialRecController
