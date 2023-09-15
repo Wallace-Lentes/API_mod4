3490c7b96b0bcac1300b5bab022483a165d82046
beafromsea <beatrizdoots@gmail.com>
2023-09-15 12:59:30 -0300

    feat: criação da entidade UsuariosModel

---
+++ src/models/UsuariosModel.js
@@ -0,0 +1,13 @@
+class UsuariosModel{
+    constructor(id, nome, email, senha, telefone, cpf, cep, numero){
+        this.id = id
+        this.nome = nome
+        this.email = email
+        this.senha = senha
+        this.telefone = telefone
+        this.cpf = cpf
+        this.cep = cep
+        this.numero = numero
+    }
+}
+export default UsuariosModel
