2729a3a8750977b3114edd1d887627c32bf2360f
wallace lentes <wallace.lentes@gmail.com>
2023-09-19 10:20:20 -0300

    refactor: ajuste de importação.

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ import express from "express";
 import MaterialRecController from "./src/controllers/MaterialRecController.js";
 import UsuariosController from "./src/controllers/UsuariosController.js";
 import ProdutosController from "./src/controllers/produtoController.js";
-import HistoricoController from "./src/controllers/HistoricoController.js";
 import CarrinhoController from "./src/controllers/carrinhoController.js";
+import HistoricoController from "./src/controllers/historicoController.js";
 
 const app = express()
 const port = process.env.PORT || 3000
