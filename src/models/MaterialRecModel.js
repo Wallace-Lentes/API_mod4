8a054b285ccbe520f2a423aa0aef5e7ba5cc8434
wallace lentes <wallace.lentes@gmail.com>
2023-09-20 22:12:16 -0300

    chore: exclusao de arquivos conflitantes.

--- src/models/MaterialRecModel.js
+++
@@ -1,8 +0,0 @@
-class MaterialRecModel {
-    constructor(tipoLixo, quantidade, peso){
-        this.tipoLixo = tipoLixo
-        this.quantidade = quantidade
-        this.peso = peso
-    }
-}
-export default MaterialRecModel
